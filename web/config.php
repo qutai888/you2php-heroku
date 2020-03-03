e5d504700d50215172e80477056f2cf6112a2fb5
qutai888 <qutai888@msn.cn>
2020-03-03 16:56:32 +0800

    Add files via upload

--- web/config.php
+++ web/config.php
@@ -1,9 +1,10 @@
 <?php
+$gl=(isset($_COOKIE['gl']) && $_COOKIE['gl'])?$_COOKIE['gl']:'HK';
 define('ROOT_PART', Root_part());
-define('APIKEY', getenv("APIKEY"));
-define('GJ_CODE', getenv("GJ_CODE"));
-define('SITE_NAME',getenv("SITE_NAME"));
-define('TITLENAME', getenv("TITLENAME"));
-define('EN2DEKEY',getenv("EN2DEKEY"));
-define('EMAIL', getenv("EMAIL"));
+define('APIKEY', 'AIzaSyAkJ4LscKKTcFBmk0rJftcRdc1BRRR8Wzw');
+define('GJ_CODE', $gl);
+define('SITE_NAME', '小曲视频');
+define('TITLENAME', '小曲视频');
+define('EN2DEKEY', '0090909i604wkkfkak');
+define('EMAIL', 'qutai888@msn.cn');
 ?>
